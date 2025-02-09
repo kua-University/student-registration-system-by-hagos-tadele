d17838eb9c3eb9ba173fe04a71db0f8489b36f96
samuel-zeray <163921005+samuel-zeray@users.noreply.github.com>
2025-02-09 09:39:26 -0800

    Add files via upload

---
+++ main_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+# from .models import Student, Course, CourseSchedule, StudentRegistration
+#
+# # Register models
+# admin_app.site.register(Student)
+# admin_app.site.register(Course)
+# admin_app.site.register(CourseSchedule)
+# admin_app.site.register(StudentRegistration)
+
